a43fc454b8c284f35d52df57d29a1516ede51105
dsFbhKj <168513490+dsFbhKj@users.noreply.github.com>
2024-07-15 12:58:11 -0700

    worked on the frames

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,25 @@
+const Overlay = () => {
+  return (
+    <>
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <div
+        style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}
+      >
+        mercy birthday collection
+      </div>
+     
+    </div>
+    </>
+  )
+}
+
+export default Overlay
